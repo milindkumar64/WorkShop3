03ac9f3d63b67f6a44421434df7053ebd7845453
milindkumar64 <milindgupta@gmail.com>
2023-04-17 15:12:03 +0530

    Add: find sum and average of numbers in arraylist using lambda expression or Stream

---
+++ src/com/bridgelabz/SumOfArraList.java
@@ -0,0 +1,39 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+
+interface Sum {
+
+	ArrayList<Integer> arrlist = new ArrayList<>();
+	
+	public void  sum(ArrayList<Integer> arrlist);
+}
+
+public class SumOfArraList {
+
+	public static void main(String[] args) {
+	
+		
+		
+		ArrayList<Integer> arrlist = new ArrayList<>();
+		arrlist.add(new Integer(4));
+		arrlist.add(new Integer(5));
+		arrlist.add(new Integer(2));
+		arrlist.add(new Integer(6));
+		arrlist.add(new Integer(7));
+		arrlist.add(new Integer(33));
+		arrlist.add(new Integer(45));
+		arrlist.add(new Integer(32));
+		arrlist.add(new Integer(89));
+		arrlist.add(new Integer(102));
+		arrlist.add(new Integer(117));
+		arrlist.add(new Integer(8));
+		arrlist.add(new Integer(10));
+		arrlist.add(new Integer(11));
+		arrlist.add(new Integer(18));
+
+		
+		
+	}
+
+}
